50b33228af9c85d184b3a8a84cbaa0b60a3507c9
RaulDiaz <rulhdiaz@gmail.com>
2023-06-07 09:03:37 -0600

    update

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box, Flex, HStack, Link, IconButton, Icon, Text, useDisclosure, Button, Stack, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { GiTechnoHeart } from 'react-icons/gi'
+
+export default function Navbar() {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems='center' justifyContent='space-between' >
+        <IconButton 
+          size='md' 
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon /> }
+          display={{md: 'none'}}
+          onClick={isOpen ? onClose : onOpen}
+          />
+
+        <HStack>
+          <Link as={ReactLink} to='/'>
+            <Flex alignItems='center'>
+              <Icon as={GiTechnoHeart} h={6} w={6} color='orange.400' />
+              <Text fontWeight='extrabold'>Tech Lines</Text>
+            </Flex>
+          </Link>
+        </HStack>
+      </Flex>
+    </Box>
+  );
+}
