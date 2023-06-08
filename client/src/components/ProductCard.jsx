bcef255cf7f51c4f6052a33a577d00be6442b830
RaulDiaz <rulhdiaz@gmail.com>
2023-06-08 08:40:45 -0600

    product card detail

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -31,7 +31,38 @@ export default function ProductCard({product}){
                 {product.stock <= 0 && <Circle size='10px' position='absolute' top={2} right={2} bg='red.300' />}
                 <Image src={product.image} alt={product.name} roundedTop='lg' />
 
-                <Box></Box>
+                <Box flex='1' maxH='5' alignItems='baseline'>
+                    {product.stock <= 0 && (
+                        <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='red'>
+                            Sold out
+                        </Badge>
+                    )}
+                    {product.isNew && (
+                        <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='green'>
+                            New
+                        </Badge>
+                    )}
+                </Box>
+                <Flex mt='1' justifyContent='space-between' alignContent='center'>
+                    <Link as={ReactLink} to={`/product${product._id}`} pt='2' cursor='pointer'>
+                        <Box fontSize='2xl' fontWeight='semibold' lineHeight='tight'>
+                            {product.name}
+                        </Box>
+                    </Link>
+                </Flex>
+                <Flex justify='space-between'>
+                    <Box fontSize='2xl' color={useColorModeValue('gray.800', 'white')}>
+                        <Box as='span' color='gray.600' fontSize='lg'>
+                            $
+                        </Box>
+                        {product.price.toFixed(2)}
+                    </Box>
+                    <Tooltip label='Add to cart' bg='white' placement='top' color='gray.800' fontSize='1.2em'>
+                        <Button variant='ghost' display='flex' disabled={product.stock <= 0}>
+                            <Icon as={FiShoppingCart} h={7} w={7} alignSelf='center' />
+                        </Button>
+                    </Tooltip>
+                </Flex>
         </Stack>
     );
 }
