92d36e527074340556dfb2507337cf217fdd6a79
RaulDiaz <rulhdiaz@gmail.com>
2023-06-09 08:57:06 -0600

    reviews

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -16,6 +16,24 @@ import { FiShoppingCart } from 'react-icons/fi';
 import { Link as ReactLink } from 'react-router-dom';
 import { StarIcon } from '@chakra-ui/icons';
 
+const Rating = ({ rating, numReviews }) => {
+    const { iconSize, setIconSize } = useState('14px');
+    return (
+        <Flex>
+            <HStack spacing='2px'>
+                <StarIcon size={iconSize} w='14px' color='orange.500' />
+                <StarIcon size={iconSize} w='14px' color={rating >= 2 ? 'orange.500' : 'gray.200' } />
+                <StarIcon size={iconSize} w='14px' color={rating >= 3 ? 'orange.500' : 'gray.200' } />
+                <StarIcon size={iconSize} w='14px' color={rating >= 4 ? 'orange.500' : 'gray.200' } />
+                <StarIcon size={iconSize} w='14px' color={rating >= 5 ? 'orange.500' : 'gray.200' } />
+            </HStack>
+            <Text fontSize='md' fontWeight='bold' ml='4px'>
+                {`${numReviews} ${numReviews === 1 ? 'Review' : 'Reviews'}`}
+            </Text>
+        </Flex>
+    );
+}
+
 export default function ProductCard({product}){
     return (
         <Stack p='2' 
@@ -49,6 +67,9 @@ export default function ProductCard({product}){
                             {product.name}
                         </Box>
                     </Link>
+                </Flex>
+                <Flex justifyContent='space-between' alignContent='center' py='2'>
+                    <Rating rating={product.rating} numReviews={product.numReviews} />
                 </Flex>
                 <Flex justify='space-between'>
                     <Box fontSize='2xl' color={useColorModeValue('gray.800', 'white')}>
