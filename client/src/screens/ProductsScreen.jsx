852dc76e9a3575a0994250de9e1a6dd299b487e7
RaulDiaz <rulhdiaz@gmail.com>
2023-06-14 07:32:00 -0600

    spinner

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Center, Wrap, WrapItem } from '@chakra-ui/react';
+import { Center, Wrap, WrapItem, Spinner, Stack, Alert, AlertIcon, AlertTitle, AlertDescription } from '@chakra-ui/react';
 import ProductCard from '../components/ProductCard';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../redux/actions/productActions';
@@ -18,13 +18,24 @@ const ProductsScreen = () => {
     // console.log(products)
     return (
         <Wrap spacing='30px' justify='center' minHeight='100vh'>
-            {products.map((product) => (
+            {loading ? (
+            <Stack direcion='row' spacing={4}>
+                <Spinner mt={20} thickness='2px' speed='0.65s' emptyColor='gray.200' color='orange.500' size='xl' />
+            </Stack>
+            ) : error ? (
+            <Alert status='error'>
+                <AlertIcon />
+                <AlertTitle>We are sorry!</AlertTitle>
+                <AlertDescription>{error}</AlertDescription>
+            </Alert>
+            ) : products.map((product) => (
                 <WrapItem key={product._id}>
                     <Center w='250px' h='550px'>
                         <ProductCard product={product} /> 
                     </Center>
                 </WrapItem>
             ))}
+            
         </Wrap>
     );
 }
