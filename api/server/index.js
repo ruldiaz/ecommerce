96ba24ea34568c4f5f0129229d8b274bcf9889a3
RaulDiaz <rulhdiaz@gmail.com>
2023-07-05 09:24:56 -0600

    paypal

--- api/server/index.js
+++ api/server/index.js
@@ -7,6 +7,7 @@ import cors from 'cors';
 // Routes
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config();
 connectToDatabase();
@@ -24,6 +25,7 @@ const port = process.env.PORT || 3001;
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.listen(port, ()=>{
   console.log(`Server listening on Port ${port}.`);
---
+++ client/src/redux/slices/order.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const initialState = {
+  loading: false,
+  error: null,
+  shippingAddress: null,
+  orderInfo: null,
+};
+
+export const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {
+    setLoading: (state) => {
+      state.loading = true;
+    },
+    setError: (state, {payload}) => {
+      state.error = payload;
+      state.loading = false;
+    },
+    shippingAddressAdd: (state, {payload})=>{
+      state.shippingAddress = payload;
+      state.loading = false;
+    },
+  }
+})
+
+export const { setLoading, setError, shippingAddressAdd  } = orderSlice.actions;
+export default orderSlice.reducer;
+export const orderSelector = (state) => state.order;
