62f827524dceaf2e32ca3ed203be159b9fb33375
RaulDiaz <rulhdiaz@gmail.com>
2023-07-11 09:41:58 -0600

    reviews complete

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { setProducts, setLoading, setError, setProduct } from '../slices/products';
+import { setProducts, setLoading, setError, setProduct, productReviewed, resetError } from '../slices/products';
 
 export const getProducts = () => async (dispatch) => {
   dispatch(setLoading(true));
@@ -28,4 +28,37 @@ export const getProduct = (id) => async(dispatch) => {
       )
     )
   }
+}
+
+export const createProductReview = (productId, userId, comment, rating, title) => async(dispatch, getState) => {
+  dispatch(setLoading(true));
+  const { 
+    user: {userInfo},
+   } = getState();
+
+   try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        'Content-Type': 'application/json',
+      },
+    }
+    const { data } = await axios.post(`http://localhost:3001/api/products/reviews/${productId}`, {comment, userId, rating, title}, config);
+    localStorage.setItem('userInfo', JSON.stringify(data));
+    dispatch(productReviewed());
+   } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message 
+        ? error.response.data.message 
+        : error.message 
+        ? error.message 
+        : 'An unexpected error has occurred. Please try again later.'
+      )
+    )
+   }
+}
+
+export const resetProductError = () => async (dispatch) => {
+  dispatch(resetError());
 }
--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -1,28 +1,40 @@
 import React, { useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Image, Text, Wrap, Stack, Spinner, Alert, AlertIcon, AlertDescription, AlertTitle, Flex, Badge, Heading, HStack, Button, SimpleGrid, useToast } from '@chakra-ui/react';
+import { Box, Image, Text, Wrap, Stack, Spinner, Alert, AlertIcon, AlertDescription, AlertTitle, Flex, Badge, Heading, HStack, Button, SimpleGrid, useToast, Tooltip, Textarea, Input, } from '@chakra-ui/react';
 import { MinusIcon, StarIcon, SmallAddIcon } from '@chakra-ui/icons';
 import { BiPackage, BiCheckShield, BiSupport } from 'react-icons/bi';
 import { useDispatch, useSelector } from 'react-redux';
-import { getProduct } from '../redux/actions/productActions';
+import { getProduct, resetProductError } from '../redux/actions/productActions';
 import { addCartItem } from '../redux/actions/cartActions';
-
+import { createProductReview } from '../redux/actions/productActions';
 
 export default function ProductScreen(){
+  const [comment, setComment] = useState('');
+  const [rating, setRating] = useState(1);
+  const [title, setTitle] = useState('');
+  const [reviewBoxOpen, setReviewBoxOpen] = useState(false);
   const [ amount, setAmount ] = useState(1);
   let { id } = useParams();
   const toast = useToast();
   //redux
   const dispatch = useDispatch();
   const products = useSelector(state => state.products);
-  const { loading, error, product } = products;
+  const { loading, error, product, reviewSend } = products;
 
   const cartContent = useSelector(state => state.cart);
   const { cart } = cartContent;
 
+  const user = useSelector(state => state.user);
+  const {userInfo} = user;
+
   useEffect(()=>{
     dispatch(getProduct(id));
-  }, [dispatch, id, cart]);
+    if(reviewSend){
+      toast({description: 'Product review saved.', status: 'success', isClosable: true});
+      dispatch(resetProductError());
+      setReviewBoxOpen(false);
+    }
+  }, [dispatch, id, cart, reviewSend]);
 
   const changeAmount = (input) => {
     if (input === 'plus' && amount < product.stock) {
@@ -31,6 +43,12 @@ export default function ProductScreen(){
     if (input === 'minus' && amount > 1) {
       setAmount(amount - 1);
     }
+  }
+
+  const hasUserReviewed = () => product.reviews.some((item)=>item.user === userInfo._id);
+
+  const onSubmit = () => {
+    dispatch(createProductReview(product._id, userInfo._id, comment, rating, title));
   }
 
   const addItem = ()=>{
@@ -124,6 +142,43 @@ export default function ProductScreen(){
               <Image mb='30px' src={product.image} alt={product.name} />
           </Flex>
           </Stack>
+          {userInfo && (
+            <>
+              <Tooltip label={hasUserReviewed() ? 'You have already reviewed this product.' : ''} fontSize='md'>
+                <Button isDisabled={hasUserReviewed()} my='20px' w='140px' colorScheme='orange' onClick={()=>setReviewBoxOpen(!reviewBoxOpen)}>
+                  Write a review
+                </Button>
+              </Tooltip>
+              {reviewBoxOpen &&
+              <Stack mb='20px'>
+                <Wrap>
+                  <HStack spacing='2px'>
+                    <Button variant='outline' onClick={()=>setRating(1)}>
+                      <StarIcon color='orange.500' />
+                    </Button>
+                    <Button variant='outline' onClick={()=>setRating(2)}>
+                      <StarIcon color={rating >= 2 ? 'orange.500' : 'gray.200'} />
+                    </Button>
+                    <Button variant='outline' onClick={()=>setRating(3)}>
+                      <StarIcon color={rating >= 3 ? 'orange.500' : 'gray.200'} />
+                    </Button>
+                    <Button variant='outline' onClick={()=>setRating(4)}>
+                      <StarIcon color={rating >= 4 ? 'orange.500' : 'gray.200'} />
+                    </Button>
+                    <Button variant='outline' onClick={()=>setRating(5)}>
+                      <StarIcon color={rating >= 5 ? 'orange.500' : 'gray.200'} />
+                    </Button>
+                  </HStack>
+                </Wrap>
+                <Input onChange={(e)=> setTitle(e.target.value)} placeholder='Review title (optional)' />
+                <Textarea onChange={(e)=> setComment(e.target.value)} placeholder={`The ${product.name} is...`} />
+                <Button w='140px' colorScheme='orange' onClick={()=>onSubmit()}>
+                  Publish review
+                </Button>
+              </Stack>
+              }
+            </>
+          )}
           <Stack>
             <Text fontSize='xl' fontWeight='bold'>
               Reviews
