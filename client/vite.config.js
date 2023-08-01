06d6c606dd87c9ea300bb15cef1bc3d5494229ad
RaulDiaz <rulhdiaz@gmail.com>
2023-08-01 10:28:33 -0600

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   // }
   build: {
     rollupOptions: {
-      external: [], // Remove 'axios' from this list
+      external: ['axios'], // Remove 'axios' from this list
     },
   }
 })
